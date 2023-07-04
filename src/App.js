23a08a241791af9190bbc78514d7318da531662a
unknown <tanmayghai20@gmail.com>
2023-07-04 23:28:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Task02 from "./components/Task02";
+function App() {
+  return (
+    <div className="App">
+      <Task02 />
+    </div>
+  );
+}
+
+export default App;
